b5bd1f0b3922a1a3f2b739cf7b0be8d93e807116
visiosfera <ermano@gmail.com>
2025-03-09 09:13:04 -0300

    Add project files.

---
+++ BlazorAppTecladoVirtual/Pages/AttributeItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAppTecladoVirtual.Pages
+{
+    internal class AttributeItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string ValueList { get; set; }
+        public string DefaultValue { get; set; }
+    }
+}
